2c6d9046e41301e0498554bf0a880b53efce5fe4
batmandoescalc <matthew.lamoureux@uconn.edu>
2026-01-13 16:18:20 -0500

    added tested files

---
+++ mod02_build_bot_predictor.py
@@ -0,0 +1,21 @@
+# packages
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+
+# set seed
+seed = 314
+
+def train_model(X, y, seed=seed):
+    """
+    Build a GBM on given data
+    """
+    model = GradientBoostingClassifier(
+        learning_rate=0.1,
+        n_estimators=100,
+        max_depth=8,
+        subsample=1,
+        min_samples_leaf=1,
+        random_state=seed
+    )
+    model.fit(X, y)
+    return model
